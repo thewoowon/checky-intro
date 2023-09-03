cc0ec03fe9b52eab9c245ce734befc5bd61f5ddf
thewoowon <thewoowon@naver.com>
2023-09-03 15:22:05 +0900

    feat: add youtube link

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function HomePage() {
           <div className="">
             <YouTube
               //videoId : https://www.youtube.com/watch?v={videoId} 유튜브 링크의 끝부분에 있는 고유한 아이디
-              videoId={"-pSJ4yosnfQ"}
+              videoId={"sdVOFjJEN04"}
               //opts(옵션들): 플레이어의 크기나 다양한 플레이어 매개 변수를 사용할 수 있음.
               //밑에서 더 설명하겠습니다.
               opts={{
